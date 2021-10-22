a951bfffafc043e2e68e54765ab049255b3f2590
Sheikh Muhammad Saeed <sheikh.saeed@concordia.ca>
2021-10-22 14:08:07 -0400

    FinalCommit

--- labCode.ino
+++ labCode.ino
@@ -28,7 +28,7 @@ void loop() {
  }
  else if(resVal > 204 && resVal < 408) 
  {
-  digitalWrite(red1, LOW);
+  digitalWrite(red1, HIGH);
   digitalWrite(yellow1, HIGH);
   digitalWrite(yellow2, LOW);
   digitalWrite(yellow3, LOW);
@@ -36,26 +36,26 @@ void loop() {
  }
   else if(resVal > 408 && resVal < 612) 
  {
-  digitalWrite(red1, LOW);
-  digitalWrite(yellow1, LOW);
+  digitalWrite(red1, HIGH);
+  digitalWrite(yellow1, HIGH);
   digitalWrite(yellow2, HIGH);
   digitalWrite(yellow3, LOW);
   digitalWrite(red2, LOW);
  }
   else if(resVal > 612 && resVal < 816) 
  {
-  digitalWrite(red1, LOW);
-  digitalWrite(yellow1, LOW);
-  digitalWrite(yellow2, LOW);
+  digitalWrite(red1, HIGH);
+  digitalWrite(yellow1, HIGH);
+  digitalWrite(yellow2, HIGH);
   digitalWrite(yellow3, HIGH);
   digitalWrite(red2, LOW);
  }
  else  
  {
-  digitalWrite(red1, LOW);
-  digitalWrite(yellow1, LOW);
-  digitalWrite(yellow2, LOW);
-  digitalWrite(yellow3, LOW);
+  digitalWrite(red1, HIGH);
+  digitalWrite(yellow1, HIGH);
+  digitalWrite(yellow2, HIGH);
+  digitalWrite(yellow3, HIGH);
   digitalWrite(red2, HIGH);
  }
 }
